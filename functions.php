5aba1abb5dd3aebc706b901e190274663bad2c0a
Alena Piskareva <a.piskareva98@yandex.ru>
2020-10-24 22:06:13 +0300

    4. functions.php : подключила bootstrap

--- functions.php
+++ functions.php
@@ -2,4 +2,5 @@
     add_action( 'wp_enqueue_scripts', 'theme_add_scripts' );
     function theme_add_scripts() {
         wp_enqueue_style('style', get_stylesheet_uri());
+        wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap-grid.min.css');
     }
