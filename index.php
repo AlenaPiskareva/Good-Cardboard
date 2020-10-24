988cf87f488de70ebdc28122fe4cf74d70002e29
Alena Piskareva <a.piskareva98@yandex.ru>
2020-10-24 22:11:31 +0300

    index.php : удалила подключение slick

--- index.php
+++ index.php
@@ -5,8 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Добрый картон</title>    
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
-    <link href="slick/slick.css" rel="stylesheet">
-    <link href="slick/slick-theme.css" rel="stylesheet">
     <link href="css/index.css" rel="stylesheet">
     
     <?php wp_head(); ?>
