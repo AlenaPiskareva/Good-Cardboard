02178391fd8a496e6324b9e2b87afadda654556f
Alena Piskareva <a.piskareva98@yandex.ru>
2020-10-24 22:08:57 +0300

    7. functions.php : подключила файл animate.css

--- functions.php
+++ functions.php
@@ -3,4 +3,5 @@
     function theme_add_scripts() {
         wp_enqueue_style('style', get_stylesheet_uri());
         wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap-grid.min.css');
+        wp_enqueue_style('animate', get_template_directory_uri() . '/css/animate.css');
     }
