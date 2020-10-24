365a4e7c693cac2000e92f3d519ac081c7f2f465
Alena Piskareva <a.piskareva98@yandex.ru>
2020-10-24 22:03:56 +0300

    3. Подправила структуру для файла functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,5 @@
 <?php
-    add_action( 'wp_enqueue_script', 'theme_name_scripts' );
-    function theme_name_scripts(){
-        wp_enqueue_style('style', get_stylesheet_uri());        
+    add_action( 'wp_enqueue_scripts', 'theme_add_scripts' );
+    function theme_add_scripts() {
+        wp_enqueue_style('style', get_stylesheet_uri());
     }
-?>
